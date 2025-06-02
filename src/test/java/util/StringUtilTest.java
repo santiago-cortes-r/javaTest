8febada5a8fbc48edd60ad8c3f9b3dd723b09468
santiago-cortes <santiagocortes202@gmail.com>
2025-06-02 11:04:52 -0500

    test for exceptions

--- src/test/java/util/StringUtilTest.java
+++ src/test/java/util/StringUtilTest.java
@@ -4,16 +4,30 @@ package util;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
-
-
-        assertEquals("holaholahola", StringUtil.repeat("hola", 3) );
+    public void repeat_string_once() {
         assertEquals("hola", StringUtil.repeat("hola", 1) );
     }
 
+    @Test
+    public void repeat_string_mutiple_times() {
+        assertEquals("holaholahola", StringUtil.repeat("hola", 3) );
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        assertEquals("", StringUtil.repeat("hola", 0) );
+    }
+
+    @Test
+    public void repeat_string_negative_times() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            StringUtil.repeat("hola", -1);
+        });
+    }
 
 }
