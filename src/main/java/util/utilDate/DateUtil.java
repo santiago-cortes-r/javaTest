cafa4040ca0e46cb169e8fd8f649de99dbd5c14c
santiago-cortes <santiagocortes202@gmail.com>
2025-06-05 10:43:24 -0500

    isLeapYear method (TDD)

--- src/main/java/util/utilDate/DateUtil.java
+++
@@ -1,9 +0,0 @@
-package util.utilDate;
-
-public class DateUtil {
-
-    public static boolean isLeapYear(int year){
-        return false;
-    }
-
-}
---
+++ src/main/java/util/utilDate/DateYearUtil.java
@@ -0,0 +1,10 @@
+package util.utilDate;
+
+public class DateYearUtil {
+
+    public static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+
+}
